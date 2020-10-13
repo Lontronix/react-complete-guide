d39faf5b1ee773d5ee3ce589b58a370ff91ee256
Lonnie Gerol <lonniegerol@gmail.com>
2020-10-13 00:53:45 -0400

    3.32 Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+// import components with uppercase so that they don't collide with default tags like div, li, etc.
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World.</h1>
+         <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//components are just javascript functions that return some jsx
+const person = () => {
+    return <p>I'm a Person!</p>
+}; 
+
+export default person;
